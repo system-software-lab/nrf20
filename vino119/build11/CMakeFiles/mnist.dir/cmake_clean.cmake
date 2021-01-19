283778ab3e6883a04dd562868f5eb95b236b727a
pinguin-der-bellt <76509344+pinguin-der-bellt@users.noreply.github.com>
2021-01-19 21:39:58 +0900

    temp

---
+++ vino119/build11/CMakeFiles/mnist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mnist.dir/main.cpp.o"
+  "armv7l/Release/mnist.pdb"
+  "armv7l/Release/mnist"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mnist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

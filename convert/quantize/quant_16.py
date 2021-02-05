6f5e8fc104a7e685a4413557ff25c13c399d98a6
pinguin-der-bellt <76509344+pinguin-der-bellt@users.noreply.github.com>
2021-02-05 12:51:22 +0900

    Update and rename quant16.py to quant_16.py

---
+++ convert/quantize/quant_16.py
@@ -0,0 +1,7 @@
+#quant_16.py
+
+import tensorflow as tf
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tf.float16]
+tflite_quant_model = converter.convert()
